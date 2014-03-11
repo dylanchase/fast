41b5ae5273450ee882151b133675fea21c25cc7d
dylanchase <dylanchase@gmail.com>
2014-03-11 12:03:25 -0400

    Update run_fast.m

--- run_fast.m
+++ run_fast.m
@@ -6,8 +6,8 @@ file_name = 'Sample_FAST';
 in_file = [file_name '.fst'];
 out_file = [file_name '.out'];
 
-write_fast_file(in_file, fast_dir); % 3
+write_fast_file(in_file, fast_dir);
 
-exec_fast(in_file, fast_dir);  % 1
+exec_fast(in_file, fast_dir);
 
-[out_data, out_list] = read_fast_output(out_file, fast_dir); % 2
+[out_data, out_list] = read_fast_output(out_file, fast_dir);
